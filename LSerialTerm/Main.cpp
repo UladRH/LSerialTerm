608d1f201193b9d34f256490f5424ea31e18e4fb
Uladzislau Hlebovich <uladzislau@sviet.me>
2020-03-18 22:13:57 +0300

    Qt5 Hello World

---
+++ LSerialTerm/Main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include <QApplication>
+#include <QFile>
+#include <QtWidgets/QStyleFactory>
+
+#include "MainWindow.h"
+
+int main(int argc, char *argv[]) {
+    qunsetenv("QT_DEVICE_PIXEL_RATIO");
+    qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", QByteArray("1"));
+
+    QApplication qtApp(argc, argv);
+
+    qtApp.setStyle(QStyleFactory::create("Fusion"));
+
+    QFile File(":Stylesheet.qss");
+    if (File.open(QFile::ReadOnly)) {
+        QString StyleSheet = QLatin1String(File.readAll());
+        qtApp.setStyleSheet(StyleSheet);
+    }
+
+    MainWindow mainWindow;
+    mainWindow.show();
+
+    return qtApp.exec();
+}
